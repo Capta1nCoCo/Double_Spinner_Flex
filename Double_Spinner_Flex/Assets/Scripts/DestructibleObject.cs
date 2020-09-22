0af4330ea411762e7e8cce68f945d1c1f6c56581
Capta1nCoCo <femabu28@gmail.com>
2020-09-22 19:29:01 +0300

    Spinner Objects, SpinnerController, Explosion OnTriggerEnter

---
+++ Double_Spinner_Flex/Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    [Header("Explosion Properties")]
+    [SerializeField] float explosionForceMin = 500f;
+    [SerializeField] float explosionForceMax = 500f;
+    [SerializeField] float explosionRadius = 30f;
+
+    Rigidbody[] allRigidBodies;
+
+    private void Awake()
+    {
+        allRigidBodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+    
+
+    void Update()
+    {
+        //For Testing Purposes
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(allRigidBodies.Length > 0)
+            {
+                foreach(Rigidbody rigidbody in allRigidBodies)
+                {
+                    rigidbody.AddExplosionForce(Random.Range(explosionForceMin, explosionForceMax), transform.position, explosionRadius);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            foreach (Rigidbody rigidbody in allRigidBodies)
+            {
+                rigidbody.AddExplosionForce(Random.Range(explosionForceMin, explosionForceMax), transform.position, explosionRadius);
+            }
+        }
+    }
+}
