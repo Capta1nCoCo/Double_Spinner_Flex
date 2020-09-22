3172f6d6e12d94f4ddf04c6636c6d4da6614d5c3
Capta1nCoCo <femabu28@gmail.com>
2020-09-22 17:47:35 +0300

    Explosion, Color Palette, Spinner Pack

---
+++ Double_Spinner_Flex/Assets/DestructibleObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    [Header("Explosion Properties")]
+    [SerializeField] float explosionForce = 500f;
+    [SerializeField] float explosionRadius = 2f;
+
+    Rigidbody[] allRigidBodies;
+
+    private void Awake()
+    {
+        allRigidBodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //For Testing Purposes
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(allRigidBodies.Length > 0)
+            {
+                foreach(Rigidbody rigidbody in allRigidBodies)
+                {
+                    rigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+                }
+            }
+        }
+    }
+}
