785ab9b37bbe09aa59b1ca0979ab6bfea86d27b1
Capta1nCoCo <femabu28@gmail.com>
2020-09-23 22:03:20 +0300

    Screen Boundaries For Spinner Movement (Done)

--- Double_Spinner_Flex/Assets/Scripts/SpinnerController.cs
+++ Double_Spinner_Flex/Assets/Scripts/SpinnerController.cs
@@ -7,13 +7,13 @@ public class SpinnerController : MonoBehaviour
     [SerializeField] Transform spinnerRed;
     [SerializeField] Transform spinnerYellow;
     [SerializeField] float speedModifier = 0.015f;
-    [SerializeField] float zRayOffset = 30f;
+    [SerializeField] float zRayOffset = 35f;
     
-    Touch touch;    
+    Touch touch;
 
-    float xMin, xMax, yMin, yMax;
-    Ray xMinRay, xMaxRay, zMinRay, zMaxRay;
-    float xMinInWorld, xMaxInWorld, zMinInWorld, zMaxInWorld;
+    float xPadding = 8f;    
+    float zMin, zMax;
+    
 
     // Update is called once per frame
     void Update()
@@ -47,36 +47,22 @@ public class SpinnerController : MonoBehaviour
         var rawXPos = spinnerRed.position.x + deltaX;
         var rawZPos = spinnerRed.position.z + deltaZ;
 
-        float clampedXPos = Mathf.Clamp(rawXPos, xMin, xMax);
-        float clampedZPos = Mathf.Clamp(rawZPos, zMinInWorld, zMaxInWorld);
+        float clampedXPos = Mathf.Clamp(rawXPos, -xPadding, xPadding);
+        float clampedZPos = Mathf.Clamp(rawZPos, zMin, zMax);
 
-        spinnerRed.position = new Vector3(rawXPos, spinnerRed.position.y, clampedZPos);
+        spinnerRed.position = new Vector3(clampedXPos, spinnerRed.position.y, clampedZPos);
     }
 
     private void SetUpMoveBoundaries()
     {
-        // updated coordinates for X and Z in the Word of spinners related to the Screen
+        // for Z movement
         Camera gameCamera = Camera.main;
-        
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+                
+        float yMinToWorld = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;      
 
-        xMinRay = gameCamera.ScreenPointToRay(new Vector3(xMin, 0, 0));
-        xMaxRay = gameCamera.ScreenPointToRay(new Vector3(xMax, 0, 0));
-
-        zMinRay = gameCamera.ScreenPointToRay(new Vector3(0, 0, yMin));
-        zMinInWorld = zMinRay.origin.z;
-        zMaxInWorld = zMinRay.origin.z + zRayOffset;
-
-
-        Debug.Log("Z min: " + xMinRay.origin.z + "Z Max: " + xMaxRay.origin.z);
-    }
-
-    public float TestingRayPosition()
-    {
-        return zMinRay.origin.z;
-    }
+        Ray zMinRay = gameCamera.ScreenPointToRay(new Vector3(0, 0, yMinToWorld));
+        zMin = zMinRay.origin.z;
+        zMax = zMinRay.origin.z + zRayOffset;
+    }   
 
 }
