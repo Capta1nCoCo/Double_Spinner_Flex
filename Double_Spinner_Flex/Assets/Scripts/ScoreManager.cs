9f25170468eb5791d120b6964f36f05ae8421b51
Capta1nCoCo <femabu28@gmail.com>
2020-09-24 16:20:38 +0300

    Testing PopUp Socre Text

---
+++ Double_Spinner_Flex/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    
+    int score;   
+
+    private void Start()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int amount)
+    {
+        score += amount;
+        scoreText.text = score.ToString();
+    }
+}
---
+++ Double_Spinner_Flex/Assets/Scripts/ScorePopup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScorePopup : MonoBehaviour
+{
+    [SerializeField] TextMeshPro scorePopupPrefab;
+    [SerializeField] float flyUpSpeed = 20f;
+
+    int minScoreNum = 100;
+    int maxScoreNum = 200;
+
+    ScoreManager scoreManager;
+
+    private void Awake()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
+    public void PopUpScoreNumber()
+    {
+        var scorePopUp = Instantiate(scorePopupPrefab, transform.position, Quaternion.identity);
+        int scoreValue = Random.Range(minScoreNum, maxScoreNum);
+        scorePopUp.text = "+" + scoreValue.ToString();
+        scorePopUp.transform.Translate(Vector3.up * flyUpSpeed * Time.deltaTime);
+        scoreManager.AddToScore(scoreValue);
+        Destroy(scorePopUp.gameObject, 1f);
+    }
+}
