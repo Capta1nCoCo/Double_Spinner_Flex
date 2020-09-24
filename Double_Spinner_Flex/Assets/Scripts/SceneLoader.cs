0ac209fc70191f6cc8ce68ebe65ded5c14f5d3aa
Capta1nCoCo <femabu28@gmail.com>
2020-09-24 15:31:02 +0300

    Scenes, Menus & Scene Loader

---
+++ Double_Spinner_Flex/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 3f;
+
+    int currentSceneIndex;
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadNextSceneWithDelay());
+        }
+    }
+
+    IEnumerator LoadNextSceneWithDelay()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadCreditsScreen()
+    {
+        SceneManager.LoadScene("Credits Screen");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
